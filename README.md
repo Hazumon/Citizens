1a06019ad7a90cc9d8db8e98c8adcb70267fefa0
Zerell <jackafella2@gmail.com>
2019-11-12 13:19:49 -0700

    updated stylesheets

---
+++ README.md
@@ -0,0 +1,5 @@
+**** Instructions ***
+
+1.) Install the latest version of Node.js
+2.) Run $ npm install or $ npm update for your initial installation or if your adding or updated files
+3.)
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import CatSection from './CatSection';
 const App = props => {
   return(
     <div>
-      <h1>Floofs of Launch</h1>
+      <h1>Citizens</h1>
       <DogSection
       dogs={props.animals.dogs} // pulls data from the animals variable that was passed
                                 // from the  main.js and sends it to the DogSection file with the dogs var

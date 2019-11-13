7b26461690e0daf2d6d0dcf73c4bd7f08ad5c9cb
Zerell <jackafella2@gmail.com>
2019-11-12 23:47:19 -0700

    resolving babel issues

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './stylesheets/main.scss';
-import 'babel-polyfill';
+// import 'babel-polyfill'; 
+import '.././node_modules/babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import animals from './constants/data';
 import App from './components/app'
 
 ReactDOM.render(
-  <App animals={animals}/>, document.getElementById('app')
+  <App animals={animals} />, document.getElementById('app')
   // passing in the data from animals from the data.js file to a variable called animals.
 );
